7e87aa43f494f29104a8555bd570cad45e3ded91
Sam Bertillo <bertillosam9@gmail.com>
2025-07-31 07:08:44 +0800

    added files for navigation components.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link, NavLink} from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel} from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+const Sidebar = () => {
+  const activeMenu = true;
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    </div>
+  )
+}
+
+export default Sidebar
